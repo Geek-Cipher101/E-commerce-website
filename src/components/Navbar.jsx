c6e89c7611604eb5deab995086f767d8b22b51f0
Akshat-Cipher010 <akshatnigam7931@gmail.com>
2025-04-25 22:50:22 +0530

    Refactor Navbar component to integrate search functionality and improve code readability
    - Added context for search visibility using ShopContext.
    - Simplified JSX structure by removing unnecessary line breaks and improving formatting.
    - Enhanced image elements with onClick handlers for better interactivity.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,62 +1,47 @@
-import React, { useState } from 'react';
-import { assets } from '../assets/assets';
-import { Link, NavLink } from 'react-router-dom';
+import React, { useContext, useState } from "react";
+import { assets } from "../assets/assets";
+import { Link, NavLink } from "react-router-dom";
+import { ShopContext } from "../context/ShopContext";
 
 const Navbar = () => {
   const [visible, setVisible] = useState(false);
+
+  const {setShowSearch} = useContext( ShopContext);
 
   return (
     <div className="flex items-center justify-between py-5 font-medium">
       {/* Logo */}
       <Link to="/">
-        <img
-          src={assets.logo}
-          className="w-36"
-          alt="Logo"
-        />
+        <img src={assets.logo} className="w-36" alt="Logo" />
       </Link>
 
       {/* Navigation Menu */}
       <ul className="hidden sm:flex gap-5 text-sm text-gray-700 ">
-        <NavLink
-          to="/"
-          className="flex flex-col items-center gap-1"
-        >
+        <NavLink to="/" className="flex flex-col items-center gap-1">
           <p>HOME</p>
           <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
         </NavLink>
 
-        <NavLink
-          to="/collection"
-          className="flex flex-col items-center gap-1"
-        >
+        <NavLink to="/collection" className="flex flex-col items-center gap-1">
           <p>COLLECTION</p>
           <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
         </NavLink>
 
-        <NavLink
-          to="/about"
-          className="flex flex-col items-center gap-1"
-        >
+        <NavLink to="/about" className="flex flex-col items-center gap-1">
           <p>ABOUT</p>
           <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
         </NavLink>
 
-        <NavLink
-          to="/contact"
-          className="flex flex-col items-center gap-1"
-        >
+        <NavLink to="/contact" className="flex flex-col items-center gap-1">
           <p>CONTACT</p>
           <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
         </NavLink>
       </ul>
 
       <div className="flex items-center gap-6">
-        <img
-          src={assets.search_icon}
-          className="w-5 cursor-pointer"
-          alt=""
-        />
+        <img 
+          onClick={() => setShowSearch(true)}
+          src={assets.search_icon} className="w-5 cursor-pointer" alt="" />
 
         <div className="group relative">
           <img
@@ -72,15 +57,8 @@ const Navbar = () => {
             </div>
           </div>
         </div>
-        <Link
-          to="/cart"
-          className="relative"
-        >
-          <img
-            src={assets.cart_icon}
-            className="w-5 min-w-5"
-            alt=""
-          />
+        <Link to="/cart" className="relative">
+          <img src={assets.cart_icon} className="w-5 min-w-5" alt="" />
           <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]">
             10
           </p>
@@ -95,7 +73,7 @@ const Navbar = () => {
       {/* sidebar menu for small screens */}
       <div
         className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${
-          visible ? 'w-full' : 'w-0'
+          visible ? "w-full" : "w-0"
         }`}
       >
         <div className="flex flex-col text-gray-600">
@@ -103,10 +81,7 @@ const Navbar = () => {
             onClick={() => setVisible(false)}
             className="flex items-center gap-4 p-3 cursor-pointer"
           >
-            <img
-              className="h-4 rotate-180"
-              src={assets.dropdown_icon}
-            />
+            <img className="h-4 rotate-180" src={assets.dropdown_icon} />
             <p>Back</p>
           </div>
           <NavLink
