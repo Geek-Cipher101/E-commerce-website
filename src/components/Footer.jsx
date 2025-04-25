ba28cf0e0e394232098536009196c37d6767e491
Akshat-Cipher010 <akshatnigam7931@gmail.com>
2025-04-25 16:43:56 +0530

    Add frontend files and initial setup

---
+++ src/components/Footer.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { assets } from "../assets/assets";
+
+const Footer = () => {
+  return (
+    <div className="">
+      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
+        <div>
+          <img src={assets.logo} alt="logo" className="w-32 mb-5" />
+          <p className="w-full md:w-2/3 text-gray-600">
+            Your one-stop shop for the best products at unbeatable prices. We
+            are committed to providing quality and value to our customers.
+          </p>
+        </div>
+        <div>
+          <p className="text-xl font-medium mb-5">COMPANY</p>
+          <ul className="flex flex-col gap-1 text-gray-600">
+            <li>HOME</li>
+            <li>ABOUT US</li>
+            <li>DELIVERY</li>
+            <li>PRIVACY</li>
+          </ul>
+        </div>
+
+        <div>
+          <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
+          <ul className="flex flex-col gap-1 text-gray-600">
+            <li>+1-212-456-7890</li>
+            <li>contact@forever_you.com</li>
+          </ul>
+        </div>
+      </div>
+
+      <div>
+        <hr />
+        <p className="text-center text-medium py-5">
+          © 2025@Forever.com - All rights reserved.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/LatestCollection.jsx
@@ -0,0 +1,44 @@
+import React, { useContext, useState, useEffect } from "react";
+import { ShopContext } from "../context/ShopContext";
+import Title from "./Title";
+import ProductItem from "./ProductItem";
+
+const LatestCollection = () => {
+  const { products } = useContext(ShopContext);
+
+  //console.log(products); // Use the variable to avoid the unused variable error
+  const [latestProducts, setLatestProducts] = useState([]);
+
+  useEffect(() => {
+    setLatestProducts(products.slice(0, 10));
+  }, []);
+
+  return (
+    <div className="my-10">
+      <div className="text-center py-8 text-3xl">
+        <Title text1={"LATEST"} text2={"COLLECTIONS"} />
+        <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">
+          We will build the admin dashboard of this eCommerce website so that
+          admin can upload the project, delete the product or check all products
+          added on the store. We will build this eCommerce backend project /
+          Backend API's using Node.js and Express and all products, user and
+          order data will be stored on MongoDB database.
+        </p>
+      </div>
+      {/* Rendering Products */}
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+        {latestProducts.map((item, index) => (
+          <ProductItem
+            key={index}
+            id={item._id}
+            image={item.image}
+            name={item.name}
+            price={item.price}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default LatestCollection;
